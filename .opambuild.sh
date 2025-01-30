38fddaac955d40c89d0ef577fe53c765f45bb3d6
Anil Madhavapeddy <anil@recoil.org>
2025-01-30 12:57:56 +0000

    fix

--- .opambuild.sh
+++ .opambuild.sh
@@ -4,6 +4,6 @@
 export OPAMROOT=`pwd`/_opamroot
 export OPAMYES=1
 export OPAMCONFIRMLEVEL=unsafe-yes
-opam init -ny
+opam init -ny --disable-sandboxing
 opam switch create . 
 opam exec -- dune build --profile=release
